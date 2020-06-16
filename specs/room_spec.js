b64a187e822515e4ef0b621dded0b4ad75d1009b
Rumen Roger <rumenroger@gmail.com>
2020-06-16 16:41:32 +0100

    Room: should start not painted

--- specs/room_spec.js
+++ specs/room_spec.js
@@ -13,4 +13,9 @@ describe('Room', function () {
     assert.strictEqual(actual, 10)
   })
 
+  it('should start not painted', function () {
+    const actual = room.painted
+    assert.strictEqual(actual, false)
+  })
+  
 })
