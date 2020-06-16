0b15a66f07db4e8e5ed0fa3aa449bf88bb48fc49
Rumen Roger <rumenroger@gmail.com>
2020-06-16 17:10:47 +0100

    Paint: should have a number of litres of paint

--- specs/paint_spec.js
+++ specs/paint_spec.js
@@ -8,6 +8,11 @@ describe('Paint', function () {
     paint = new Paint(5)
   })
 
+  it('should have a number of litres of paint', function () {
+    const actual = paint.capacity
+    assert.strictEqual(actual, 5)
+  })
+
   it('should be able to check if it is empty', function () {
     const actual = paint.empty
     assert.strictEqual(actual, false)
