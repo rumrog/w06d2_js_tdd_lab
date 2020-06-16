43cdf6d75a498e3d4207fe733d6ba84bf77d74c0
Rumen Roger <rumenroger@gmail.com>
2020-06-16 17:26:50 +0100

    Decorator constructor

---
+++ decorator.js
@@ -0,0 +1,6 @@
+const Room = require('./room.js')
+const Paint = require('./paint.js')
+
+const Decorator = function (stock) {
+  this.stock = []
+}
---
+++ specs/decorator_spec.js
@@ -0,0 +1,8 @@
+const assert = require('assert')
+const Decorator = require('../decorator.js')
+
+describe('Decorator', function () {
+  let decorator
+
+  beforeEach
+})
