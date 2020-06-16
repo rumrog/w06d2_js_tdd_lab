0a69a5f8452c919cf38539cfc53c1cc98d1fd5dd
Rumen Roger <rumenroger@gmail.com>
2020-06-16 17:36:02 +0100

    Removed stock array. Just integers now.

--- decorator.js
+++ decorator.js
@@ -2,5 +2,7 @@ const Room = require('./room.js')
 const Paint = require('./paint.js')
 
 const Decorator = function (stock) {
-  this.stock = []
+  this.stock = stock
 }
+
+module.exports = Decorator
--- specs/decorator_spec.js
+++ specs/decorator_spec.js
@@ -4,5 +4,18 @@ const Decorator = require('../decorator.js')
 describe('Decorator', function () {
   let decorator
 
-  beforeEach
+  beforeEach(function () {
+    decorator = new Decorator(0)
+  })
+
+  it('should start with an empty paint stock', function () {
+    const actual = decorator.stock
+    assert.deepEqual(actual, 0)
+  })
+
+  // it('should be able to add a can of paint to paint stock', function () {
+  //   decorator.addPaint()
+  //   const actual = decorator.stock
+  //   assert.deepStrictEqual(actual, 1)
+  // })
 })
